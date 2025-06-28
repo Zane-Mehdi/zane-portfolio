5bf668568b265a090fad85161ef76d95f4d4e922
zane <zanemehdi6@gmail.com>
2025-06-28 02:01:28 +0100

    update portfolio

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,7 +7,6 @@ export const Navbar = ({ theme, toggleTheme, currentView, setCurrentView }) => {
     const navLinks = [
         { name: 'Home', view: 'home' },
         { name: 'Journey', view: 'journey' },
-        { name: 'Contact', view: 'contact', isAI: true }
     ];
 
     return (
